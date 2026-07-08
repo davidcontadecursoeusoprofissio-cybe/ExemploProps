87d4333dacf8cb6fc55af8274e9bf205052446b6
davidcontadecursoeusoprofissio-cybe <davidcontadecursoeusoprofissio@gmail.com>
2026-07-08 09:21:45 -0300

    mudei

--- aulaprops/app/Components/Nav.jsx
+++ aulaprops/app/Components/Nav.jsx
@@ -2,7 +2,7 @@
 
 export default function Menu(){
     return(
-        <nav className="flex gap-20 mb-20 bg-blue-700  h-25 text-red-800">
+        <nav className="flex gap-20 mb-20 bg-blue-700  h-25 text-black-800">
             <li>Home</li>
             <li>Anotações</li>
             <li>Usuarios</li>
