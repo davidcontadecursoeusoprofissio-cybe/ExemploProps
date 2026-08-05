519c7d2ac236c5304f66ef65f359d111a3e9a44f
davidcontadecursoeusoprofissio-cybe <davidcontadecursoeusoprofissio@gmail.com>
2026-08-05 10:00:18 -0300

    atualiza

--- aulaprops/app/api/users/route.js
+++ aulaprops/app/api/users/route.js
@@ -5,3 +5,20 @@ import sqlite3 from 'sqlite'
 import {open} from 'sqlite'
 
 import path from "path";
+
+async function abrirBanco(){
+    return open({
+        filename: path.json(process.cwd(),'database.db'),
+        driver: sqlite3.Database
+
+    })
+
+}
+
+export async function GET(){
+    const db = await abrirBanco();
+
+    const usuarios = await db.all('SELECT * FROM users')
+
+    return NextResponse.json(usuarios)
+}
